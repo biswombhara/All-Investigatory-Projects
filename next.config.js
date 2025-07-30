81ee4a6d7827595e9409417e0fe180278e78fdbc
Screen Studio <studscreen.io@gmail.com>
2025-07-30 14:10:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -23,12 +23,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'static.vecteezy.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
