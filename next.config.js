9bb10d7ec855db28ef8e1f4b4e147ce149d1bf2e
Screen Studio <studscreen.io@gmail.com>
2025-07-30 14:15:41 +0000

    add this image as math bg image

--- next.config.js
+++ next.config.js
@@ -23,12 +23,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
