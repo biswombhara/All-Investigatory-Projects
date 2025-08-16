6945ae2f3de843d85a24550a998dc794582b370f
Screen Studio <studscreen.io@gmail.com>
2025-08-16 19:29:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -74,7 +74,6 @@ export default function RootLayout({
         <ThemeProvider
             attribute="class"
             defaultTheme="light"
-            enableSystem
             disableTransitionOnChange
         >
           <AuthProvider>
