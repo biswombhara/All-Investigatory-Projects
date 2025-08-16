27b71899cc11485b8d67b177295a387a032d717f
Screen Studio <studscreen.io@gmail.com>
2025-08-16 19:28:55 +0000

    set default to bright mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -73,7 +73,7 @@ export default function RootLayout({
       <body className={cn('min-h-screen bg-background font-body antialiased')}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
